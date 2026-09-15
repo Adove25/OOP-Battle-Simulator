c1c61a21f341279021ef4400e452ba49ad2d993e
Adove25 <avery.dove@ascte.org>
2026-09-15 14:50:24 -0500

    began hero
    ;oi

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "Random ahh stadium"
 
@@ -12,11 +12,17 @@ def main():
 
     goblin = Goblin("THRAGG THE DESTROYER")
     goblin2 = Goblin("bob")
+    hero = Hero("Dude Person")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
-    print("But no hero has answered the call... yet.")
+    print(f"The hero {hero.name} enters the arena with {hero.health} health.")
 
+    attacknum = hero.attack()
+    goblin.take_damage(attacknum)
+
+    
+    
 
 if __name__ == "__main__":
     main()
